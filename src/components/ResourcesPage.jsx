fa1315f9e66c0621d2c750c96ac6ea568196ce83
MdNihal005 <mdnihal.private1@gmail.com>
2024-03-04 18:15:14 +0530

    updated resource page

--- src/components/ResourcesPage.jsx
+++ src/components/ResourcesPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import TuF from "./Resources_DSA/TuF";
+import Codechef from "./Resources_DSA/Codechef";
+import Medium from "./Resources_DSA/Medium";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 function ResourcesPage() {
-  return <div>ResourcesPage</div>;
+  return (
+    <>
+      <Typography variant="h5" backgroundColor="dark" margin={5}>
+        <Button color="Heading" variant="contained">
+          Data Structures and Algorithms
+        </Button>
+      </Typography>
+      <Box margin={2} sx={{ width: "97%", flexGrow: 1 }}>
+        <Grid overflow={"hidden"} container gap={5}>
+          <Grid itemScope>
+            <Item>
+              <TuF />
+            </Item>
+          </Grid>
+          <Grid item>
+            <Item>
+              <Codechef />
+            </Item>
+          </Grid>
+          <Grid item>
+            <Item>
+              <Medium />
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 }
 
 export default ResourcesPage;
